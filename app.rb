a04a9cb59a69b62e798a87140b96cd865ec87dfb
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-24 14:05:28 -0500

    commit tree a30c3448403d620fe46098673ba503cee1d3602e

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
+configure do
+    enable :sessions
+    set :session_secret, "donttellmom"
+  end 
+
 class App < Sinatra::Base
+
+    get "/" do
+        erb :index
+    end
+
+    post "/checkout" do
+        session[:item] = params[:item]
+        @session = session
+        erb :show
+    end
+
 end
---
+++ models/item.rb
@@ -0,0 +1,7 @@
+class Item 
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+end
